51c46d0e1fb20659d104008c560abf108421832f
ke11i <kelli.j.biz@gmail.com>
2022-05-22 03:22:46 +0900

    [LEARN] React and JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last name: {props.lastName}</h2>
+      <h3>Age: {props.age}</h3>
+    </>
+  )
+}
+
+const App = () => {
+  const name = 'kelli';
+  const isNull = null;
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    // happening to render
+    alert("You've changed the counter to "+counter);
+    //setCounter(100);
+  }, [counter])
+
+  // JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world! I'm {name}, this is my first react app!!
+
+      {
+        isNull ?
+          (
+            <> not null </>
+          )
+          :
+          (
+            <><h1>null</h1>
+              <p>ha</p></>
+          )
+      }
+
+      <Person name="Kelli" lastName="Jung" age={28} />
+      <Person name="John" lastName="Doe" age={24} />
+
+      <button onClick={() => setCounter((prevCounter) => prevCounter - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>+</button>
     </div>
   );
 }
